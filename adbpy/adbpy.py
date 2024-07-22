57022322a7379334c7f00086da7c1c3ed839988a
Emptylight <1378990254@qq.com>
2024-07-22 20:45:54 +0800

    Add files via upload

--- adbpy/adbpy.py
+++ adbpy/adbpy.py
@@ -7,12 +7,14 @@ import time
 # 帮助菜单
 def helpMenu():
     """    \t  \t  \t\t\t  \t  \t \t\t  \t  \t """
-    print("allow_args\tallow_value\tdescription")
+    print("allow_args\t\tallow_value\t\tdescription")
     print("--mode/-m\t\t(None)\t\t\tDefault launch mode")
-    print("\t\t\t\tsetting\t\t\tConfigure settings")
-    print("--name/-n\t\t$profile_name\tThe profile name you want to launch")
+    print("\t\t\tsetting\t\t\tConfigure settings")
+    print("--name/-n\t\t$profile_name\t\tThe profile name you want to launch")
     print("-help\t\t\t(None)\t\t\tShow help menu")
     print("--help/-h\t\t(None)\t\t\tShow default help menu")
+    print("--list/-l\t\t(None)\t\t\tShow all profiles")
+    print("--version/-v\t\t(None)\t\t\tShow current version")
     print("Notification:")
     print("1.\t模拟器需要支持adb连接方式")
     print("2.\t你需要让程序能够读取到adb,要么是添加环境变量,要么把adb和本程序放在同一个文件夹下")
@@ -87,6 +89,12 @@ def launch(emulator, port, wait_time=10):
     time.sleep(int(wait_time))
     os.system(f"adb connect 127.0.0.1:{port}")
 
+def showCurrentVersion():
+    print("Current adbpy's version IS " + VERSION)
+    print("当前的adbpy的版本号是" + VERSION)
+
+VERSION = "V2"
+
 # 主函数
 if __name__ == "__main__":
     # 读取参数
@@ -94,6 +102,8 @@ if __name__ == "__main__":
     parser.add_argument("--mode", "-m", help="这是程序的运行模式")
     parser.add_argument("--name", "-n", help="输入配置文件名启动相应配置文件")
     parser.add_argument("-help", help="获取帮助列表", action="store_true")
+    parser.add_argument("--list", "-l", help="获取所有配置文件的名称", action="store_true")
+    parser.add_argument("--version", "-v", help="显示当前应用的版本号", action="store_true")
     args = parser.parse_args()
 
     # 读取配置参数
@@ -136,6 +146,13 @@ if __name__ == "__main__":
         launch(setting[args.name]["emulator"], setting[args.name]["port"], setting[args.name]["time"])
     elif args.help:
         helpMenu()
+    elif args.list:
+        print("目前的配置文件有：")
+        for i in setting.keys():
+            print("name: " + i, end=", ")
+            print("port: " + setting[i]["port"])
+    elif args.version:
+        showCurrentVersion()
     else:
         print("请选择要执行的操作:")
         mode = int(input("1.设置\n2.启动\n3.帮助\n4.退出\n:"))

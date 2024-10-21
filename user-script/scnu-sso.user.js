dc57db7821fdea4a98f00825e65f405bb2a17fdc
Emptylight <1378990254@qq.com>
2024-10-21 18:34:07 +0800

    fix: Extra white space warning

--- user-script/scnu-sso.user.js
+++ user-script/scnu-sso.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SCNU moodle sso redirect
 // @namespace    https://github.com/lingfengyu-dreaming/release/blob/main/user-script/scnu-sso.user.js
-// @version      1.0.2
+// @version      1.0.3
 // @description  自动跳转到统一登录界面
 // @author       lingfengyu-dreaming
 // @match        https://moodle.scnu.edu.cn/login/index.php
@@ -9,7 +9,7 @@
 // @grant        none
 // ==/UserScript==
 
-window.onload =  function () {
+window.onload = function () {
     var count = 0;
     try {
         redirect();

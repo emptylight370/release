fce2ba05be7f5f30a1dec6b39f5f2f074ce6b4c6
Emptylight <1378990254@qq.com>
2024-10-21 19:05:49 +0800

    feat: simulate user clicking button

--- user-script/scnu-sso.user.js
+++ user-script/scnu-sso.user.js
@@ -1,29 +1,24 @@
 // ==UserScript==
 // @name         SCNU moodle sso redirect
 // @namespace    https://github.com/lingfengyu-dreaming/release/blob/main/user-script/scnu-sso.user.js
-// @version      1.0.3
+// @version      1.0.4
 // @description  自动跳转到统一登录界面
 // @author       lingfengyu-dreaming
 // @match        https://moodle.scnu.edu.cn/login/index.php
+// @match        https://jwxt.scnu.edu.cn/xtgl/login_slogin.html
 // @icon         https://sso.scnu.edu.cn/AccountService/static/fullscreen/images/scnulogo-icon.png
 // @grant        none
 // ==/UserScript==
 
 window.onload = function () {
-    var count = 0;
-    try {
-        redirect();
-    }
-    catch {
-        if (count < 30) {
-            setTimeout(redirect, 100);
-            count++;
-        }
-    }
+    redirect();
 };
 
 function redirect() {
-    var btn = document.querySelector("#ssobtn");
-    var href = btn.href;
-    location.href = href;
+    if (location.href.includes("moodle")) {
+        var btn = document.querySelector("#ssobtn");
+    } else if (location.href.includes("jwxt")) {
+        var btn = document.querySelector("#tysfyzdl");
+    }
+    btn.click();
 }

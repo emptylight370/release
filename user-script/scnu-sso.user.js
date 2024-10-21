eba031bb86b5699dc8c0682b28816cdaff6021ec
Emptylight <1378990254@qq.com>
2024-10-21 18:31:27 +0800

    Update scnu-sso.user.js

--- user-script/scnu-sso.user.js
+++ user-script/scnu-sso.user.js
@@ -1,25 +1,24 @@
 // ==UserScript==
 // @name         SCNU moodle sso redirect
 // @namespace    https://github.com/lingfengyu-dreaming/release/blob/main/user-script/scnu-sso.user.js
-// @version      1.0.0
+// @version      1.0.1
 // @description  自动跳转到统一登录界面
 // @author       lingfengyu-dreaming
 // @match        https://moodle.scnu.edu.cn/login/index.php
 // @icon         https://sso.scnu.edu.cn/AccountService/static/fullscreen/images/scnulogo-icon.png
 // @grant        none
-// @run-at       document-idle
 // ==/UserScript==
 
-(function() {
-    var errCount=0;
+window.onload =  function () {
     try {
         redirect();
-    } catch(e) {
-        errCount++;
-        if(errCount<3)
-            setTimeout(redirect,1000);
+        console.log("redirect");
     }
-})();
+    catch {
+        setTimeout(redirect, 100);
+        console.log("no href?")
+    }
+};
 
 function redirect() {
     var btn = document.querySelector("#ssobtn");

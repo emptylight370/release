7149a24e0ce08af186f594428b4ef9824b104acb
Emptylight <1378990254@qq.com>
2025-06-28 23:55:18 +0800

    Update nexusmods.user.js

--- user-script/nexusmods.user.js
+++ user-script/nexusmods.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Better Nexus Mods
 // @namespace    https://github.com/emptylight370/release/blob/main/user-script/nexusmods.user.js
-// @version      1.2.1
+// @version      1.3.0
 // @description  Try to do something on nexusmods website.
 // @author       Emptylight
 // @match        https://www.nexusmods.com/*
@@ -12,26 +12,36 @@
 (function() {
   'use strict';
 
-  for (var run = 0; run < 5; run++){
-    var hasAD = removeADs();
-    if (hasAD) {
-      setTimeout(removeADs,3000);
-    }
-  }
+  removeADs();
+  setTimeout(checkProblems,3000);
 })();
+
+window.addEventListener("popstate", checkProblems());
 
 function removeADs() {
   let adsContainer = document.querySelector(".ads");
+  let flag = false;
   if (!adsContainer) {
     adsContainer = document.querySelector("[data-testid=\"ad-container-player\"]");
     if (adsContainer) {
       adsContainer.parentElement.parentElement.removeChild(adsContainer.parentElement);
-      return true;
-    } else {
-      return false;
+      flag = true;
     }
   } else {
     adsContainer.parentElement.removeChild(adsContainer);
-    return true;
+    flag = true;
+  }
+  if (flag) {
+    setTimeout(removeADs,3000);
+  }
+}
+
+function checkProblems() {
+  var center = document.querySelector(".text-center");
+  console.log(center);
+  if (center?.firstChild?.textContent === "Oops!") {
+    location.reload();
+    console.log("error occurs! reload page!");
+    setTimeout(checkProblems,3000);
   }
 }

db5207badd603b4f4cbcbeb706bf7dab940eaf03
Emptylight <1378990254@qq.com>
2026-04-14 17:31:37 +0800

    Update itzmx.user.js

--- user-script/itzmx.user.js
+++ user-script/itzmx.user.js
@@ -1,70 +1,56 @@
 // ==UserScript==
 // @name         Itzmx签到脚本
 // @namespace    https://github.com/emptylight370/release/blob/main/user-script/itzmx.user.js
-// @version      1.0.0
+// @version      1.1.0
 // @description  自动完成itzmx论坛的签到
 // @author       Emptylight
 // @match        https://bbs.itzmx.com/*
 // @icon         http://itzmx.com/favicon.ico
-// @grant        none
+// @grant        GM_setValue
+// @grant        GM_getValue
 // @run-at       document-end
+// @require https://scriptcat.org/lib/513/2.1.0/ElementGetter.js#sha256=aQF7JFfhQ7Hi+weLrBlOsY24Z2ORjaxgZNoni7pAz5U=
 // ==/UserScript==
 
 (async function () {
   "use strict";
 
+  const lastrun = GM_getValue("lastrun", "0000");
+  const day = new Date().getDate().toString().padStart(2, "0");
+  const month = new Date().getMonth().toString().padStart(2, "0");
+  const today = month + day;
+  if (lastrun === today) {
+    return;
+  }
+
   // Your code here...
-  var check = await getElement("qiandao");
+  const check = await elmGetter.get("#qiandao", document, 10000);
   if (check) {
-    var time = new Date().getMilliseconds();
-    var id = time % 9;
-    var emo;
+    const time = new Date().getMilliseconds();
+    const id = time % 9;
+    let emo;
     if (id == 0) {
-      emo = await getElement("kx");
+      emo = await elmGetter.get("#kx");
     } else if (id == 1) {
-      emo = await getElement("ng");
+      emo = await elmGetter.get("#ng");
     } else if (id == 2) {
-      emo = await getElement("ym");
+      emo = await elmGetter.get("#ym");
     } else if (id == 3) {
-      emo = await getElement("wl");
+      emo = await elmGetter.get("#wl");
     } else if (id == 4) {
-      emo = await getElement("nu");
+      emo = await elmGetter.get("#nu");
     } else if (id == 5) {
-      emo = await getElement("ch");
+      emo = await elmGetter.get("#ch");
     } else if (id == 6) {
-      emo = await getElement("fd");
+      emo = await elmGetter.get("#fd");
     } else if (id == 7) {
-      emo = await getElement("yl");
+      emo = await elmGetter.get("#yl");
     } else if (id == 8) {
-      emo = await getElement("shuai");
+      emo = await elmGetter.get("#shuai");
     }
     emo.click();
-    var say = await getElement("todaysay");
-    var day = new Date().getDate().toString().padStart(2, "0");
+    const say = await elmGetter.get("#todaysay");
     say.value = "签到qd" + day;
+    GM_setValue("lastrun", today);
   }
 })();
-
-async function getElement(id, timeout = 5000) {
-  return new Promise((resolve, reject) => {
-    var startTime = Date.now();
-
-    var checkElement = () => {
-      var element = document.getElementById(id);
-
-      if (element) {
-        resolve(element);
-        return;
-      }
-
-      if (Date.now() - startTime >= timeout) {
-        reject(new Error(`Timeout waiting for element: ${id}`));
-        return;
-      }
-
-      setTimeout(checkElement, 100);
-    };
-
-    checkElement();
-  });
-}
